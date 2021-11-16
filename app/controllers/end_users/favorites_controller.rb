faa80a7d253fa1505a505d8691943b8f379b28c4
niwanekochan <pooonda.0607@gmail.com>
2021-11-16 19:28:48 +0000

    お気に入り機能実装

--- app/controllers/end_users/favorites_controller.rb
+++ app/controllers/end_users/favorites_controller.rb
@@ -1,2 +1,21 @@
 class EndUsers::FavoritesController < ApplicationController
+  def index
+    user = current_user
+    @item = Item.find_by(item_id: @item.id)
+    @favorites = Favorite.where(user_id: user.id).all
+  end
+
+  def create
+    item = Item.find(params[:item_id])
+    favorite = current_user.favorites.new(item_id: item.id)
+    favorite.save
+    redirect_to end_users_item_path(item.id)
+  end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    favorite = current_user.favorites.find_by(item_id: item.id)
+    favorite.destroy
+    redirect_to end_users_item_path(item.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   attachment :image
   belongs_to :genre
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
